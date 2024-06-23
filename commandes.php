40777ade059cf1b11ec1d7928c64f9912816fbcc
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-06-23 23:14:28 +0200

    feat(commandes.php): debut de la page commande

--- commandes.php
+++ commandes.php
@@ -47,18 +47,32 @@ if (count($_GET) > 0) {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     <style>
-            body{
-            background: url(./img/blob.svg);
+        body {
+            background: url(./img/blob.jpg);
             background-size: cover;
         }
     </style>
 </head>
 
+<?php
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+    define('MAILHOST', "smtp.gmail.com");
+    $subject = "New contact request: " . $_POST['objet'];
+    $message = $_POST['prenom'] . " " . $_POST['nom'] . "souhaite commander voici la description : " . $_POST['description'];
+    $headers = 'From: info@website.com' . "\r\n" .
+        'Reply-To: ' . $_POST['email'] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    echo mail('clarisse.petitot@gmail.com', $subject, $message, $headers);
+
+    die('Thank you for your email');
+}
+?>
+
 <body class="min-h-full relative">
 
     <?php
     require './components/navbar.php';
-    require './components/filters.php';
     require './components/form.php';
     require './components/footer.php';
     ?>
@@ -66,4 +80,5 @@ if (count($_GET) > 0) {
 
 
 </body>
+
 </html>
--- components/form.php
+++ components/form.php
@@ -1,46 +1,105 @@
-<div class="h-screen w-screen flex items-center justify-center bg-stone-100">
-    <div class="p-10 rounded shadow-sm bg-stone-50 max-w-lg w-2/3">
-        <div class="mb-6 p-10 bg-white -m-10">
-            <h1 class="font-bold text-2xl text-stone-300">Commande personnalisée</h1>
+<form class="p-20" method='POST'>
+    <div class="space-y-12">
+        <div class="border-b border-gray-900/10 pb-12">
+            <h2 class="text-base font-semibold leading-7 text-gray-900 lg:text-4xl dark:text-white text-3xl lg:leading-9 leading-7 text-gray-800 font-semibold">Passer une commande</h2>
+
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="sm:col-span-4">
+                    <label for="objet" class="block text-sm font-medium leading-6 text-gray-900">Objet</label>
+                    <div class="mt-2">
+                        <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                            <input type="text" name="objet" id="objet" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-span-full">
+                    <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description précise de la commande</label>
+                    <div class="mt-2">
+                        <textarea id="description" name="description" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                    </div>
+                </div>
+
+            </div>
         </div>
-        <form>
-            <div class="grid grid-cols-2 gap-6">
-                <div class="flex flex-col mb-4">
-                    <label>Nom
-                        <span class="block text-xs font-light text-stone-400">instructions ici lorem ipsum</span>
-                    </label>
-                    <input type="text" placeholder="Votre nom" class="mt-2 px-4 py-2 shadow rounded"/>
+
+        <div class="border-b border-gray-900/10 pb-12">
+            <h3 class="text-xl font-semibold leading-7 text-gray-900">Informations personnelles</h3>
+
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div class="sm:col-span-3">
+                    <label for="nom" class="block text-sm font-medium leading-6 text-gray-900">Nom</label>
+                    <div class="mt-2">
+                        <input type="text" name="nom" id="nom" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
                 </div>
-                <div class="flex flex-col mb-4">
-                    <label>Prénom
-                        <span class="block text-xs font-light text-stone-400">instructions ici lorem ipsum</span>
-                    </label>
-                    <input type="text" placeholder="Votre prénom" class="mt-2 px-4 py-2 shadow rounded"/>
+
+                <div class="sm:col-span-3">
+                    <label for="prenom" class="block text-sm font-medium leading-6 text-gray-900">Prenom</label>
+                    <div class="mt-2">
+                        <input type="text" name="prenom" id="prenom" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-4">
+                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Adresse Email</label>
+                    <div class="mt-2">
+                        <input id="email" name="email" type="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="col-span-full">
+                    <label for="adresse" class="block text-sm font-medium leading-6 text-gray-900">Adresse Postale</label>
+                    <div class="mt-2">
+                        <input type="text" name="adresse" id="adresse" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2 sm:col-start-1">
+                    <label for="ville" class="block text-sm font-medium leading-6 text-gray-900">Ville</label>
+                    <div class="mt-2">
+                        <input type="text" name="ville" id="ville" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="codep" class="block text-sm font-medium leading-6 text-gray-900">Code Postal</label>
+                    <div class="mt-2">
+                        <input type="text" name="codep" id="codep" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
+                </div>
+
+                <div class="sm:col-span-2">
+                    <label for="pays" class="block text-sm font-medium leading-6 text-gray-900">Pays</label>
+                    <div class="mt-2">
+                        <input type="text" name="pays" id="pays" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    </div>
                 </div>
             </div>
-            <div class="flex flex-col mb-4">
-                <label>Email
-                    <span class="block text-xs font-light text-stone-400">instructions ici lorem ipsum</span>
-                </label>
-                <input type="email" placeholder="Votre Adresse email" class="mt-2 px-4 py-2 shadow rounded"/>
+        </div>
+
+        <div class="pb-12">
+            <div class="relative flex gap-x-3">
+                <div class="flex h-6 items-center">
+                    <input id="conditions" name="conditions" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                </div>
+                <div class="text-sm leading-6">
+                    <label for="conditions" class="font-medium text-gray-900">Conditions d'utilisation</label>
+                    <p class="text-gray-500">clause obligatoire</p>
+                </div>
             </div>
-            <div class="flex flex-col mb-4">
-                <label>Téléphone
-                    <span class="block text-xs font-light text-stone-400">instructions ici lorem ipsum</span>
-                </label>
-                <input type="tel" placeholder="XX XX XX XX XX" class="mt-2 px-4 py-2 shadow rounded"/>
-            </div>
-            
-            <div class="flex flex-col mb-4">
-                <label>Description précise de votre commande
-                    <span class="block text-xs font-light text-stone-400">instructions ici lorem ipsum</span>
-                </label>
-                <textarea rows="5" placeholder="..." class="mt-2 px-6 py-2 shadow rounded"></textarea>
-            </div>
-            <div class="mt-6 flex gap-6">
-                <button type="submit" class="rounded-full bg-orange-300 py-4 px-10 font-bold text-white shadow hover:bg-blue-500">Envoyer</button>  
-                <button type="submit" class="rounded-full bg-transparent py-4 px-10 font-bold text-stone-400 border border-stone-200 hover:border-orange-300 hover:text-orange-300">Réinitialiser</button>  
-            </div>
-        </form>
+        </div>
     </div>
-</div>
+
+    <!-- Apply Filter Button (Large Screen) -->
+
+    <div class="hidden md:block absolute right-20 bottom-20 md:py-5 lg:px-10 md:px-5 py-5 px-4">
+                <input type="submit" value="Envoyer" class="hover:bg-gray-700 dark:bg-white dark:text-gray-800 dark:hover:bg-gray-100 focus:ring focus:ring-offset-2 focus:ring-gray-800 text-base leading-4 font-normal py-4 px-6 text-white bg-black">
+            </div>
+
+            <!-- Apply Filter Button (Table or lower Screen) -->
+
+            <div class="block md:hidden w-full mt-5">
+                <input type="submit" value="Envoyer" class="w-full hover:bg-gray-700 dark:bg-white dark:text-gray-800 dark:hover:bg-gray-100 focus:ring focus:ring-offset-2 focus:ring-gray-800 text-base leading-4 font-normal py-3 px-5 text-white bg-black">
+            </div>
+</form>
