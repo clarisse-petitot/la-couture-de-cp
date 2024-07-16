804d6bea219de695f60ac771e8545f0dcae72a99
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-16 12:41:06 +0200

    chore(): enlever ligne en trop

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil | La Couture de CP</title>
-    <link rel="stylesheet" href="style.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
