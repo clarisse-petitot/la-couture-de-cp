6a238d1c770be19db45b092d4ebb7d90c2e6dec8
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-16 12:08:00 +0200

    fix(pagination.php): background flèche à droite

--- components/pagination.php
+++ components/pagination.php
@@ -34,7 +34,7 @@ require_once 'fonctions.php';
         }
         ?>
         <a href="<?= $page ?>?id_page=<?= ($nbr_page_total) . getQueryWithoutIdPage() ?>">
-            <li><button class="flex items-center justify-center w-10 h-10 text-pink-500 transition-colors duration-150 bg-white rounded-full focus:shadow-outline hover:bg-pink-100">
+            <li><button class="flex items-center justify-center w-10 h-10 text-pink-500 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-pink-100">
                     <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
                         <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" fill-rule="evenodd"></path>
                     </svg></button>
