a29d48057644912e3bc014fe45ed6281b695a048
Clarisse Petitot <clarisse.petitot@gmail.com>
2024-07-23 19:03:22 +0200

    feat(valider.php): page validation post commande

--- valider.php
+++ valider.php
@@ -21,15 +21,15 @@
 
 <body class="min-h-full relative">
     <?php
-    require 'navbar.php';
+    require 'components/navbar.php';
     ?>
     <div class="flex flex-col p-10 md:p-0 md:flex-row gap-10 w-full justify-center items-center mt-10 pb-32">
         <div class="flex flex-col gap-12">
-            <h3 class="font-black text-6xl text-pink-500 uppercase">Merci !</h3>
+            <h3 class="font-black text-6xl text-pink-500">Merci !</h3>
             <p>
                 Votre commande a bien été envoyé
             </p>
-            <a href="../index.php" class="text-pink-500 flex gap-2 transform transition-all hover:scale-110 hover:text-pink-600 w-fit-content">
+            <a href="index.php" class="text-pink-500 flex gap-2 transform transition-all hover:scale-110 hover:text-pink-600 w-fit-content">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
                 </svg>
@@ -41,7 +41,7 @@
         </div>
     </div>
     <?php
-    require 'footer.php';
+    require 'components/footer.php';
     ?>
 
 </body>
